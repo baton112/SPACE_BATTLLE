301cb745230d31f5090547f503221c5911c1d0ef
QuinnK <kacper09@onet.eu>
2014-05-10 22:25:58 +0200

    Create game_syn.h

---
+++ GAME CLIENT/game_client/game_syn.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include "vehicle.h"
+#include <list>
+#include <iostream>
+#include <chrono>
+
+#define WIN32_LEAN_AND_MEAN
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <tchar.h>
+#include <strsafe.h>
+
+// Need to link with Ws2_32.lib
+#pragma comment(lib, "ws2_32.lib")
+
+#define MAX_USERS 4
+
+#define UNKNOWN -1
+
+#define LISTEN_PORT 50000
+
+class game
+{
+private:
+	vehicle *vehicleTab[MAX_USERS];
+	void loadTextures();
+	std::list< sf::Event> keysPressed;
+	bool vehiclesActive[MAX_USERS];
+	int ID;
+public:
+	game();
+	~game();
+	void runGameLoop(sf::RenderWindow *appWindow);
+
+};
+
