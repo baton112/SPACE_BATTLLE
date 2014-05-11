5b61867714bb8949e31ff5ec77715666cc0cb155
QuinnK <kacper09@onet.eu>
2014-05-11 15:33:44 +0200

    Update game.cpp

--- GAME SERVER/game_client/game_client/game.cpp
+++ GAME SERVER/game_client/game_client/game.cpp
@@ -1,17 +1,27 @@
 #include "game.h"
-
+//********************************************* SERWER 
 struct ThreadParam{
-	vehicle *MyVehicle;
+	vehicle **MyVehicle;
 	SOCKET Connect;
 	int clietntNumber;
+	bool * vehActive;
 };
+
+struct message{
+	int ID;
+	double X;
+	double Y;
+	double angle;
+};
+
 
 struct netowrkThreadParams{
 	vehicle ** vtab;
+	bool *vehActive;
 };
 
 
-DWORD WINAPI ThreadFunctionRecive(LPVOID lpParam)
+DWORD WINAPI ThreadSendToEverybody(LPVOID lpParam)				//wysylanie pozycji "wsyzstkich do wszystkich"
 {
 	HANDLE hStdout;
 	ThreadParam *functionParams;
@@ -21,17 +31,59 @@ DWORD WINAPI ThreadFunctionRecive(LPVOID lpParam)
 		return 1;
 
 	functionParams = (ThreadParam*)lpParam;
-
+	message * msg = new message;
 	std::cout << "Connected " << functionParams->clietntNumber << std::endl;
+	char *  buf = new char[(sizeof(message))];
+	while (true)
+	{
 
+		for (int i = 0; i < 4; i++){
+			if (functionParams->vehActive[i] && functionParams->clietntNumber != i){
+				
+				msg->ID = i;
+				msg->X = functionParams->MyVehicle[i]->position.x;
+				msg->Y = functionParams->MyVehicle[i]->position.y;
+				msg->angle = functionParams->MyVehicle[i]->angle;
+				send(functionParams->Connect, (char*)msg, sizeof(message), 0);
+			}
+		}
+		Sleep(80);
+	}
+}
+
+DWORD WINAPI ThreadFunctionRecive(LPVOID lpParam)				//odbior wiadomosci od klientow
+{
+	HANDLE hStdout;
+	ThreadParam *functionParams;
+
+	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hStdout == INVALID_HANDLE_VALUE)
+		return 1;
+
+	functionParams = (ThreadParam*)lpParam;
+	message * msg;
+	std::cout << "Connected " << functionParams->clietntNumber << std::endl;
+	char *  buf = new char[(sizeof(message))];
 	while (1)
 	{
-		char  buf;
+		
+	
 		//recev -- blokuje dalsze wykowyanie programu do nadejscia otrzymania czegos lub zamkniecia polaczenia
-		while (recv(functionParams->Connect, &buf, 1, 0) > 0) // przesylanie pojednym znaku 
+		while (sizeof(message)==recv(functionParams->Connect, buf, sizeof(message), 0)) // przesylanie pojednym znaku 
 		{
-			printf("%c", buf);
+			 msg = (message*)buf;
+			 if (msg->ID == UNKNOWN){											//jesli przyslal niewazne ID, (podłączył się), to odeslij mu ID tylko ID
+				 msg->ID = functionParams->clietntNumber;
+				 send(functionParams->Connect, (char*)&msg->ID, sizeof(int), 0);
+			 }
+			 else{
+				 functionParams->MyVehicle[msg->ID]->position.x = msg->X;		//zapisywanie odebranyc pozycji
+				 functionParams->MyVehicle[msg->ID]->position.y = msg->Y;
+				 functionParams->MyVehicle[msg->ID]->angle = msg->angle;
+			 }
 		}
+
+		std::cout << "koncze";
 	}
 
 	return 0;
@@ -67,22 +119,38 @@ DWORD WINAPI ThreadHandleConnections(LPVOID lpParam)
 	{
 		if (Connect = accept(Listen, (struct sockaddr FAR*)&server, &server_size))
 		{
-			std::cout << "CONNECTED " << std::endl;
-			vehicleTab[ConnectedClients] = new vehicle;
-			ThreadParam *a;
-			a = new ThreadParam;
-			a->MyVehicle = vehicleTab[ConnectedClients];
-			a->clietntNumber = ConnectedClients;
-			a->Connect = Connect;
-			//odpalenie watku obslugi polaczenienia przychodzacego 
-			threadsHandleTab[ConnectedClients] = CreateThread(
-				NULL,                   // default security attributes
-				0,                      // use default stack size  
-				ThreadFunctionRecive,       // thread function name
-				a,          // argument to thread function 
-				0,                      // use default creation flags 
-				NULL);   // returns the thread identifier
-			ConnectedClients++;
+			if (ConnectedClients < 3){
+				std::cout << "CONNECTED " << std::endl;
+				ConnectedClients++;
+				parametry->vehActive[ConnectedClients] =  true;
+
+
+				ThreadParam * a = new ThreadParam;				//przygotowanie parametru dla funkcji watkow
+				a->MyVehicle = vehicleTab;
+				a->clietntNumber = ConnectedClients;
+				a->Connect = Connect;
+				a->vehActive = parametry->vehActive;
+
+																//odpalenie watku obslugi polaczenienia przychodzacego 
+				threadsHandleTab[ConnectedClients] = CreateThread (
+					NULL,                   // default security attributes
+					0,                      // use default stack size  
+					ThreadFunctionRecive,       // thread function name
+					a,          // argument to thread function 
+					0,                      // use default creation flags 
+					NULL);   // returns the thread identifier
+
+				Sleep(100);										//czekaj, zeby zdazyl przydzieic ID (bez tego sa lagi)
+
+				threadsHandleTab[ConnectedClients] = CreateThread(
+					NULL,                   // default security attributes
+					0,                      // use default stack size  
+					ThreadSendToEverybody,       // thread function name
+					a,          // argument to thread function 
+					0,                      // use default creation flags 
+					NULL);   // returns the thread identifier
+			}
+		
 		}
 	}
 
@@ -100,6 +168,8 @@ void game::runGameLoop(sf::RenderWindow *appWindow)
 	HANDLE NetworkindThreadHandle;
 
 
+	this->vehiclesActive[0]=true;
+
 	int error = WSAStartup(version, &wsaData);
 	if (error != 0)
 	{
@@ -108,9 +178,13 @@ void game::runGameLoop(sf::RenderWindow *appWindow)
 		return ;
 	}
 
+
+
+
 	//////////tworzenie watku do oblugi polaczen 
 	netowrkThreadParams a;
 	a.vtab = this->vehicleTab; //przeslanie tablicy wskaznikow na pojazdy obslugiwane przez uzytkownikow
+	a.vehActive = this->vehiclesActive;
 	NetworkindThreadHandle = CreateThread(
 		NULL,                   // default security attributes
 		0,                      // use default stack size  
@@ -195,7 +269,7 @@ void game::runGameLoop(sf::RenderWindow *appWindow)
 		{
 			for (int i = 0; i < MAX_USERS; i++)
 			{
-				if (this->vehicleTab[i] != NULL)
+				if (this->vehiclesActive[i])
 					vehicleTab[i]->drowVehicle(appWindow);
 			}
 		}
@@ -210,7 +284,8 @@ game::game()
 {
 	for (int i = 0; i < MAX_USERS; i++)
 	{
-		vehicleTab[i] = NULL;
+		vehicleTab[i] = new vehicle();
+		vehiclesActive[i] = false;
 	}
 }
 
